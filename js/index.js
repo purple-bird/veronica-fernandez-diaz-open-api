5d65ea0993d83d4ae75344fae22afa9516fb9b45
Veronica <179986051+purple-bird@users.noreply.github.com>
2025-02-04 22:08:46 -0700

    created button that will show a random superhero

--- js/index.js
+++ js/index.js
@@ -1,18 +1,75 @@
-// Create fetch for my Github:
-fetch("https://api.github.com/users/purple-bird/repos")
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Request failed");
-    }
-    return response.json();
-  })
+const superheroContainer = document.getElementById("superhero-container");
 
-  // Handle JSON data:
-  .then((repositories) => {
-    console.log("These are the repositories:", repositories);
-  })
+const fetchSuperhero = document.getElementById("fetch-superhero");
 
-  // Handle errors:
-  .catch((error) => {
-    console.error("An error occured:", error);
-  });
+// The fetch happens after clicking the button
+fetchSuperhero.addEventListener("click", () => {
+  // Make sure it is empty before generating again
+  superheroContainer.innerHTML = "";
+
+  // Get random number
+  function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  let randomObject = getRandomNumber(1, 20);
+  console.log(randomObject);
+
+  // Create fetch for my Github:
+  fetch(
+    "http://gateway.marvel.com/v1/public/comics?ts=1&apikey=9509d166898bd3ba72a66cb5ad658b98&hash=148d632631f5d12900e7bf7e61af9275"
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Request failed");
+      }
+      return response.json();
+    })
+
+    // Handle JSON data:
+    .then((repositories) => {
+      // Look at superhero object
+      const superhero = repositories.data.results[randomObject];
+
+      // Look at superhero thumbnail
+      const superheroThumbnail = superhero.thumbnail;
+      console.log(superheroThumbnail);
+
+      // Create valid URL for image
+      const superheroImageURL =
+        `${superheroThumbnail.path}` + `.` + `${superheroThumbnail.extension}`;
+      console.log(superheroImageURL);
+
+      // Create image element
+      const superheroImage = document.createElement("img");
+      superheroImage.src = superheroImageURL;
+
+      // Get title
+      const title = superhero.title;
+
+      // Create header 2
+      const titleElement = document.createElement("h2");
+
+      // Put title text in header
+      titleElement.innerText = title;
+
+      // Get format
+      const format = superhero.format;
+
+      // Create header 3
+      const formatElement = document.createElement("h3");
+
+      // Put format text in header
+      formatElement.innerText = format;
+
+      // Order inside container
+      superheroContainer.appendChild(titleElement);
+      superheroContainer.appendChild(formatElement);
+      superheroContainer.appendChild(superheroImage);
+    })
+
+    // Handle errors:
+    .catch((error) => {
+      console.error("An error occured:", error);
+    });
+});
